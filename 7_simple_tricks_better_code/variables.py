bab72aa889173beb151e9555d7348f705b189a8c
RabinPhaiju <rabinphaiju15@gmail.com>
2021-05-10 22:40:06 +0545

    folder managed

--- 7_simple_tricks_better_code/variables.py
+++
@@ -1,8 +0,0 @@
-x = 10
-y =-10
-# x, y = 10, -10
-
-print(f'X ={x}, and y = {y}')
-
-x,y = y, x
-print(f'X = {x}, and y = {y}')
--- Write_better_code/list.py
+++ Write_better_code/list.py
@@ -2,13 +2,13 @@
 # enumerate ___________________________
 cities = ['bhaktapur', 'kathmandu', 'lalitpur']
 
-# for i,city in enumerate(cities):
-    # print(i, city)
+for i,city in enumerate(cities):
+    print(i, city)
    
 
 # multiple list ______zip____________
 x_list = ['bhaktapur', 'kathmandu', 'lalitpur']
 y_list = [4, 5, 6]
 
-# for x, y in zip(x_list, y_list):
-    # print(x, y)
+for x, y in zip(x_list, y_list):
+    print(x, y)
