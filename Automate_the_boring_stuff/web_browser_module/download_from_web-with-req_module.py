31ecca43b290d33944c84d36b43d12100e482004
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-10-02 07:10:59 +0545

    Chaged Path

--- Automate_the_boring_stuff/web_browser_module/download_from_web-with-req_module.py
+++ Automate_the_boring_stuff/web_browser_module/download_from_web-with-req_module.py
@@ -2,6 +2,6 @@ import requests
 
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 if not res.raise_for_status():
-    with open('RomeoAndJuliet.txt','wb') as file:
+    with open('Automate_the_boring_stuff/web_browser_module/RomeoAndJuliet.txt','wb') as file:
         for chunk in res.iter_content(100000):
             file.write(chunk)
