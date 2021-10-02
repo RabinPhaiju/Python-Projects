0eac8c461e73612794bee7d2c759ccc37cf5f3dd
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-10-02 07:19:24 +0545

    Changed Path

--- Automate_the_boring_stuff/web_scraping_with_bs4/test.py
+++ Automate_the_boring_stuff/web_scraping_with_bs4/test.py
@@ -3,7 +3,7 @@ import requests
 import re
 
 
-with open('test.html') as my_url:
+with open('Automate_the_boring_stuff/web_scraping_with_bs4/test.html') as my_url:
     page_soup = bs4.BeautifulSoup(my_url, 'lxml')
 
 containers = page_soup.find("div", {"class":"position-rel"})
